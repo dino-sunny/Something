f15486a7d89fb941de7ad06834164108f2e21910
dinosunny <dino.sunny@cabotsolutions.com>
2020-09-19 18:41:42 +0530

    Database added

--- app/src/main/java/com/dino/something/database/EmployeeDao.kt
+++ app/src/main/java/com/dino/something/database/EmployeeDao.kt
@@ -5,12 +5,12 @@ import androidx.room.Insert
 import androidx.room.Query
 import com.dino.something.module.home.Employee
 
-//@Dao
-//interface EmployeeDao {
-//    @Insert
-//    fun insert(employee: Employee)
-//
-//    @Query("SELECT * FROM employee_table")
-//    fun getEmployee(): Employee?
-//
-//}
+@Dao
+interface EmployeeDao {
+    @Insert
+    fun insert(employeeData: EmployeeData)
+
+    @Query("SELECT * FROM employee_table")
+    fun getAllEmployee(): EmployeeData?
+
+}
