e59c64359e669e0e298ad31d2c27576dfbbf1f20
dinosunny <dino.sunny@cabotsolutions.com>
2020-09-19 15:19:55 +0530

    Room Database added

---
+++ app/src/main/java/com/dino/something/database/EmployeeDao.kt
@@ -0,0 +1,16 @@
+package com.dino.something.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.dino.something.module.home.Employee
+
+@Dao
+interface EmployeeDao {
+    @Insert
+    fun insert(employee: Employee)
+
+    @Query("SELECT * FROM employee_table")
+    fun getEmployee(): Employee?
+
+}
---
+++ app/src/main/java/com/dino/something/database/EmployeeDatabase.kt
@@ -0,0 +1,36 @@
+package com.dino.something.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.dino.something.module.home.Employee
+
+
+@Database(entities = [Employee::class], version = 1,exportSchema = false)
+abstract class EmployeeDatabase : RoomDatabase() {
+
+    abstract val employeeDao: EmployeeDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: EmployeeDatabase?= null
+
+        fun getInstance(context: Context): EmployeeDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        EmployeeDatabase::class.java,
+                        "player_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
